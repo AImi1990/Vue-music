ad533ee8aa37cb5addd6b8f132e74e58611e4e06
AImi1990 <1175479211@qq.com>
2017-11-06 14:17:06 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app" @touchmove.prevent>
+    <m-header @my-event="getMyEvent"></m-header>
+    <tab></tab>
+    <keep-alive>
+      <router-view></router-view>
+    </keep-alive>
+    <player></player>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  import MHeader from 'components/m-header/m-header'
+  import Tab from 'components/tab/tab'
+  import Player from 'components/player/player'
+
+  export default {
+    methods: {
+      getMyEvent (hello) {
+        console.log('i got my event' + hello)
+      }
+    },
+    components: {
+      MHeader,
+      Tab,
+      Player
+    }
+  }
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+</style>
